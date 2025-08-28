acb8b7aeb4d419dd5f11fd442f1d913793f599d2
Varun25BCP353 <malivarun2007@gmail.com>
2025-08-28 21:46:58 +0530

    Add files via upload

---
+++ Gram to Kg.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    float i;
+    printf("Enter Value Of Weight In Grams:");
+    scanf("%f",&i);
+    printf("Value of %f Grams In kilograms:%f ",i,i/1000);
+}
---
+++ Kg To Gram.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    printf("Enter Value Of Weight In Kilograms:");
+    scanf("%d",&i);
+    printf("Value of %d Kilograms In Grams:%d ",i,i*1000);
+}
