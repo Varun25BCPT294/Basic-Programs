700a10d4808558628392acdf16775a43e132c283
Varun25BCP353 <malivarun2007@gmail.com>
2025-08-28 21:32:38 +0530

    Add files via upload

---
+++ Minutes To Hour.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    float i;
+    printf("Enter Value Of Time In Minutes:");
+    scanf("%f",&i);
+    printf("Value of %f Minutes In Hour:%f Hour",i,i*1/60);
+}
