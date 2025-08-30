0e158119cab3fd9a24f75ddf691000bf7bc8681e
Varun25BCP353 <malivarun2007@gmail.com>
2025-08-30 11:57:04 +0530

    Add files via upload

---
+++ Swap two values.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter Value Of a:\n");
+    scanf("%d",&a);
+    printf("Enter Value Of b:\n");
+    scanf("%d",&b);
+    printf("Swapped Values Of a And b is:%d,%d\n",b,a);
+}
